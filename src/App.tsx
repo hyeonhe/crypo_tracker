cd7d74eb3da7b293f0aa70d5b922bfd8b660fafb
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2023-09-05 09:59:53 +0900

    Add gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -72,7 +72,7 @@ function App() {
     <>
       <GlobalStyle />
       <Router />
-      <ReactQueryDevtools initialIsOpen={true} />
+      {/* <ReactQueryDevtools initialIsOpen={true} /> */}
     </>
   );
 }
